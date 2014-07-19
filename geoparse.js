9d21c5eee8dc9dca9ce2e22175211053e516200f
Vegard Bakke <vegard.bakke@gmail.com>
2014-07-19 20:24:44 +0200

    Initial commit

---
+++ geoparse.js
@@ -0,0 +1,186 @@
+﻿var geoparse = (function () {
+
+
+	//
+	//  [NSEW] deg [NSEW] deg [NSEW]
+	//
+
+	var _NORTH = "N";
+	var _SOUTH = "S";
+	var _EAST  = "E";
+	var _WEST = "W";
+	
+	var _dirChars = _NORTH+_SOUTH+_EAST+_WEST;
+	var _degChars = "0-9.,\"'º -";
+	var _geoRe = RegExp("\s*(["+_dirChars+"]?)\s*(["+_degChars+"]+)[,\s]*(["+_dirChars+"]?)[,\s]*(["+_degChars+"]+)\s*(["+_dirChars+"]?)");
+
+	var _self = {};
+	_self.parseGeo = function (str) {
+		var parts = _geoRe.exec(str);
+
+		if (parts != null) {
+			var dir1 = parts[1];
+			var deg1 = parts[2];
+			var dir2 = parts[3];
+			var deg2 = parts[4];
+			var dir3 = parts[5];
+		}
+		_self.dbg("<br/>"+str);
+		// If directions are placed _after_ degrees, move them
+		if (dir1 == "") {
+			dir1 = dir2;
+			dir2 = dir3;
+		}
+		// If longitude is placed before latitude, swap them
+		if (dir1 == _EAST || dir1 == _WEST) {
+			var tempDir=dir1, dir1=dir2, dir2=tempDir;
+			var tempDeg=deg1, deg1=deg2, deg2=tempDeg;
+			_self.dbg("swapping");
+		}
+		
+		// Parse the degrees string
+		latitude = _self.parseDeg(deg1);
+		longititude = _self.parseDeg(deg2);
+		
+		
+		// Adujst for negative signs
+		if (dir1 == _SOUTH) {
+			latitude = -latitude;
+		}
+		if (dir2 == _WEST) {
+			longititude = -longititude;
+		}
+		
+		var geo = [latitude, longititude]
+		return geo;
+	}
+
+	// =====================================================
+	// Parse string containing degrees, minutes and seconds
+	//
+	_self.parseDeg = function (str) {
+		_self.dbg(str);
+		if (!str) {
+			return 0.0;
+		}
+		
+		var deg = 0.0;
+		var start = 0;
+		var end = 0;
+		var sign = +1;
+
+		// Sign
+		start = _self.findSign(str, start);
+		if (start>-1) {
+			if (str[start] == "-") {
+				sign = -1;
+				start++;
+			} else if (str[start] == "+") {
+				start++;
+			}
+		}
+		_self.dbg("Sign: "+sign);
+		
+		// Degrees
+		start = _self.findNum(str, start);
+		end = _self.findDelim(str, start+1);
+		if (start>-1 && end>-1) {
+			var numStr = str.substring(start, end);
+			_self.dbg("-"+numStr+"-");
+			var num = _self.parseNum(numStr);
+			deg += num;
+		}
+		if (end == -1)	{
+			return deg;
+		}
+		
+		// Minutes
+		start = _self.findNum(str, end+1);
+		end = _self.findDelim(str, start+1);
+		if (start>-1 && end>-1) {
+			var numStr = str.substring(start, end);
+			_self.dbg("-"+numStr+"-");
+			var num = _self.parseNum(numStr);
+			deg += num/60.0;
+		}
+		if (end == -1)	{
+			return deg;
+		}
+		
+		// Seconds
+		start = _self.findNum(str, end+1);
+		end = _self.findDelim(str, start+1);
+		if (start>-1 && end>-1) {
+			var numStr = str.substring(start, end);
+			_self.dbg("-"+numStr+"-");
+			var num = _self.parseNum(numStr);
+			deg += num/3600.0;
+		}
+
+
+		return deg;
+	}
+
+	
+	
+	_self.parseNum = function (str) {
+		str = str.replace(",", ".");
+		var num = parseFloat(str);
+		return num;
+	}
+	
+	
+	_self.findSign = function (str, start) {
+		var index = start;
+		var c;
+		while (index < str.length) {
+			c = str[index];
+			if (c == "-" || c == "+" || c == "," || c == "." || (c >= "0" && c <= "9")) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	_self.findNum = function (str, start) {
+		var index = start;
+		var c;
+		while (index < str.length) {
+			c = str[index];
+			if (c == "," || c == "." || (c >= "0" && c <= "9")) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	_self.findDelim = function (str, start) {
+		var index = start;
+		var c;
+		while (index < str.length) {
+			c = str[index];
+			//if (" º'\"".indexOf(c) >- 1)
+			if (c == "," || c == "." || (c >= "0" && c <= "9")) {
+				index++;
+			} else {
+				return index;
+			}
+		}
+		return -1;
+	}
+	
+	var _dbgDiv = "";
+	_self.setDebugDiv = function (dbgDiv) {
+		_self.dbgDiv = dbgDiv;
+	}	
+	_self.dbg = function (str) {
+		if (_self.dbgDiv) {
+			var dbgDiv = document.getElementById("dbgDiv");
+			if (dbgDiv) {
+				dbgDiv.innerHTML += "<br/>" + str + "\n";
+			}
+		}
+	}
+	
+	return _self;
+}());
---
+++ geoparse_test.js
@@ -0,0 +1,45 @@
+﻿var geoparse_test = (function () {
+	// ... all vars and functions are in this scope only
+	// still maintains access to all globals
+	
+	var _self = {};
+	var _testdata = [
+		"59º 10' 12.51\" N 10º 5' 12,5\" E",
+		"59.1 N 10.5 E",
+		"59,2 S 10,5 W",
+		"E 10.5, N 59.3 ",
+		"59.4, 10.5",
+		"59º 10.51' N 10º 5' E",
+		""];
+	
+	
+	_self.createTable = function () {
+		html = ""
+		for (var i=0; i<_testdata.length; i++) {
+			html += _self.createTableRow(_testdata[i]);
+		}
+		
+		html = "<table border=1>\n" + html + "\n</table>";
+		return html;
+	}
+	
+	
+	_self.createTableRow = function (teststr) {
+		html = "";
+		
+		html += "<td>"+teststr+"</td>\n";
+		geo = geoparse.parseGeo(teststr);
+		if (geo == null) {
+			html += '<td colspan="2">null</td>';
+		} else {
+			html += "<td>"+geo[0]+"</td>\n";
+			html += "<td>"+geo[1]+"</td>\n";
+			//html += "<td>"+geo+"</td>\n";
+		}
+		html = "<tr>"+html+"</tr>";
+		return html;
+	}
+	
+	
+	return _self;
+}());
