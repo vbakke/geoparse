d2112482316b330dbdc5f068eb8debccd666e15f
Vegard Bakke <vegard.bakke@gmail.com>
2014-08-01 00:45:38 +0200

    Added UTM parse

---
+++ src/js/utmparse.js
@@ -0,0 +1,138 @@
+﻿var utmparse = (function () {
+
+
+	//
+	//  [NSEW] deg [NSEW] deg [NSEW]
+	//
+
+	var _NORTH = "N";
+	var _SOUTH = "S";
+	var _EAST  = "E";
+	var _WEST = "W";
+	
+	var _dirChars = _NORTH+_EAST+_SOUTH+_WEST;
+	var _numChars = "0-9,.";
+	var _digitChars = "0-9";
+	var _zoneBandChars = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+	var _utmRe = RegExp("\s*(["+_digitChars+"]{1,2}) *(["+_zoneBandChars+"]) *(["+_dirChars+"]?) *(["+_numChars+"]+)m? *(["+_dirChars+"]?)[, ]*(["+_dirChars+"]?) *(["+_numChars+"]+) *m? *(["+_dirChars+"]?)");
+//	var _utmRe = RegExp("\s*(["+_digitChars+"]{1,2})\s*(["+_zoneBandChars+"])(\s{0,})(.)(5)(.)");
+	//var _utmRe = RegExp("\s*(["+_digitChars+"]{1,2})\s*(["+_zoneBandChars+"])\s*(["+_dirChars+"]?)\s*");
+
+	var _self = {};
+	_self.parseUtm = function (str) {
+		var parts = _utmRe.exec(str);
+		_self.dbg("<br/><b>"+str+"</b>");
+		_self.dbg(parts);
+
+		if (parts != null) {
+			var zone  = parts[1];
+			var band  = parts[2];
+			var dir1 = parts[3];
+			var pos1  = parts[4];
+			var dir1b = parts[5];
+			var dir2 = parts[6];
+			var pos2  = parts[7];
+			var dir2b = parts[8];
+		}
+		_self.dbg("-" + zone + "-" + band + "-" + dir1 + "-" + pos1 + "-" + dir1b  + "-" + dir2 + "-" + pos2 + "-" + dir2b + "-");
+		// If directions are placed _after_ position, move them forwards
+		if (dir1 == "") {
+			dir1 = dir1b;
+		}
+		if (dir2 == "") {
+			dir2 = dir2b;
+		}
+		// If northing is placed before easting, swap them
+		if (dir1 == _NORTH || dir2 == _EAST) {
+			var tempDir=dir1, dir1=dir2, dir2=tempDir;
+			var tempPos=pos1, pos1=pos2, pos2=tempPos;
+			_self.dbg("swapping");
+		}
+		
+		// Parse the degrees string
+		easting = _self.parseNum(pos1);
+		northing = _self.parseNum(pos2);
+		
+		
+		// Adujst for negative signs
+		if (dir1 == _SOUTH) {
+			northing = -northing;
+		}
+		if (dir2 == _WEST) {
+			easting = -easting;
+		}
+		
+		var utm = [zone, band, easting, northing]
+		_self.dbg(zone + band + " " + easting + "E " + northing + "N");
+		return utm;
+	}
+
+	// =====================================================
+	// Parse string containing degrees, minutes and seconds
+	//
+
+	
+	
+	_self.parseNum = function (str) {
+		if (str) {
+			str = str.replace(",", ".");
+			var num = parseFloat(str);
+		}
+		return num;
+	}
+	
+	
+	_self.findSign = function (str, start) {
+		var index = start;
+		var c;
+		while (index < str.length) {
+			c = str[index];
+			if (c == "-" || c == "+" || c == "," || c == "." || (c >= "0" && c <= "9")) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	_self.findNum = function (str, start) {
+		var index = start;
+		var c;
+		while (index < str.length) {
+			c = str[index];
+			if (c == "," || c == "." || (c >= "0" && c <= "9")) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	_self.findDelim = function (str, start) {
+		var index = start;
+		var c;
+		while (index < str.length) {
+			c = str[index];
+			//if (" º'\"".indexOf(c) >- 1)
+			if (c == "," || c == "." || (c >= "0" && c <= "9")) {
+				index++;
+			} else {
+				return index;
+			}
+		}
+		return -1;
+	}
+	
+	var _dbgDiv = "";
+	_self.setDebugDiv = function (dbgDiv) {
+		_self.dbgDiv = dbgDiv;
+	}	
+	_self.dbg = function (str) {
+		if (_self.dbgDiv) {
+			var dbgDiv = document.getElementById("dbgDiv");
+			if (dbgDiv) {
+				dbgDiv.innerHTML += "<br/>" + str + "\n";
+			}
+		}
+	}
+	
+	return _self;
+}());
---
+++ src/jtest/utmparse_test.js
@@ -0,0 +1,68 @@
+﻿var utmparse_test = (function () {
+	
+	var _self = {};
+	var _testdata = [
+		["32V 500000mE, 2100827mN", [32, 500000, 2100827]],
+		["UTM 32V 500000, 2100827", [32, 500000, 2100827]],
+		["32 V N2100827, 500000E", [32, 500000, 2100827]],
+		["32N E500000 N2100827", [32, 500000, 2100827]],
+		["32 N2100827, 500000E", [32, 500000, 2100827]],
+		["32 N2100827N, 500000E", [32, 500000, 2100827]],
+		["" [0,0,0]]];
+	
+	
+	_self.createTable = function () {
+		html = ""
+		for (var i=0; i<_testdata.length; i++) {
+			html += _self.createTableRow(_testdata[i]);
+		}
+		
+		html = "<table border=1>\n" + html + "\n</table>";
+		return html;
+	}
+	
+	
+	_self.createTableRow = function (testdata) {
+		var teststr = testdata[0];
+		var expectedPos = testdata[1];
+		
+		html = "";
+		html += "<td>"+teststr+"</td>\n";
+		utm = utmparse.parseUtm(teststr);
+		if (utm == null) {
+			html += '<td colspan="2">null</td>';
+		} else {
+			html += "<td>"+utm[0]+"</td>\n";
+			html += "<td>"+utm[1]+"</td>\n";
+			html += "<td>"+utm[2]+"</td>\n";
+			html += "<td>"+utm[3]+"</td>\n";
+			if (expectedPos && expectedPos.length==2) {
+				html += "<td>"+_self.utmDiff(utm, expectedPos)+"</td>\n";
+			}
+		}
+		html = "<tr>"+html+"</tr>";
+		return html;
+	}
+	
+	_self.utmDiff = function (utmA, utmB) {
+		dZone = utmA[0]-utmB[0];
+		dEast = utmA[1]-utmB[1];
+		dNorth = utmA[2]-utmB[2];
+		var str = "";
+		if (Math.abs(dLat) > 0.00001) {
+			str += "Zone: "+dZone+" ";
+		}
+		if (Math.abs(dEast) > 0.00001) {
+			str += "Easting: "+dEast+" "	;
+		}
+		if (Math.abs(dNorth) > 0.00001) {
+			str += "Northing: "+dNorth+" "	;
+		}
+		if (str) {
+			str = "Diff: "+str;
+		}
+		return str
+	}
+	
+	return _self;
+}());
