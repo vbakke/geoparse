49f8dfc68c16eb2a7808e852a35a0912fa1172f7
Vegard Bakke <vegard.bakke@gmail.com>
2016-07-18 08:37:54 +0200

    HEROKU: Initial checkin

---
+++ src/heroku-side/server.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/groups", function(req, res) {
+});
+
+app.post("/groups", function(req, res) {
+    var newGroup = req.body;
+    newGroup.createdTime = new Date();
+
+    if (!(req.body.name)) {
+        handleError(res, "Invalid user input", "Must provide a name.", 400);
+    }
+
+    // Store to database
+    db.collection(CONTACTS_COLLECTION).insertOne(newGroup, function(err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new contact.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/groups/:id", function(req, res) {
+});
+
+app.put("/groups/:id", function(req, res) {
+});
+
+app.delete("/groups/:id", function(req, res) {
+});
