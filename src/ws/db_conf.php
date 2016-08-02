e69a60e3433b4f5acc882b37b21bfbf4903e2cce
Vegard Bakke <vegard.bakke@gmail.com>
2016-08-03 00:49:09 +0200

    no message

--- src/ws/db_conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-// TODO: Export settings to external config file
-$db_host="vafe.mysql.domeneshop.no";
-$db_db="vafe";
-$db_u="vafe";
-$db_p="gM735uHC";
-$db_tb_pre="geoTest";
-$environment="dev";
-?>
